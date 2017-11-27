cb61cb64ab13b13e0ff2428292b4c3a9b79a5e86
Mircea Cadariu <m.cadariu@sig.eu>
2017-11-27 14:33:55 +0100

    improved

--- directory/HelloWorld.java
+++ directory/HelloWorld.java
@@ -9,17 +9,6 @@ public class HelloWorld {
             third = first + second;
             third = first + second;
             third = first + second;
-            third = first + second;
-            third = first + second;
-            third = first + second;
-            third = first + second;
-            third = first + second;
-            third = first + second;
-            third = first + second;
-            third = first + second;
-            third = first + second;
-            third = first + second;
-            third = first + second;
 	return third;        
 }
 }
