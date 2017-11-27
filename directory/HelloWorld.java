4ba545f133026fb6177268c841beebda3063cd54
Mircea Cadariu <m.cadariu@sig.eu>
2017-11-27 14:19:52 +0100

    fixed

--- directory/HelloWorld.java
+++ directory/HelloWorld.java
@@ -3,22 +3,6 @@ package com.test;
 public class HelloWorld {
         public int add(int first, int second) {
           int third = first + second;
-                third = first + second;
-                third = first + second;
-                third = first + second;
-                third = first + second;
-                third = first + second;
-                third = first + second;
-                third = first + second;
-                third = first + second;
-                third = first + second;
-                third = first + second;
-                third = first + second;
-                third = first + second;
-                third = first + second;
-                third = first + second;
-                third = first + second;
-                third = first + second;
-          return third;
-        }
+	return third;        
+}
 }
