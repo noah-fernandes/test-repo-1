4e34a36f78e692cb1faeba81aa1c855e3e323631
Mircea Cadariu <cadariu.mircea@gmail.com>
2018-02-21 23:03:32 +0100

    Update HelloWorld.test.java

--- directory/HelloWorld.test.java
+++ directory/HelloWorld.test.java
@@ -2,8 +2,10 @@ package com.test;
 
 public class HelloWorldTest {
 
+        @Test
         public int add(int first, int second) {
           int third = first + second;
           return third;
+                assertTrue(1==1)
         }
 }
