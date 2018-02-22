e6b72fd70f54bfdaf8f1ae13a2bd6937adc0fcd9
Mircea Cadariu <cadariu.mircea@gmail.com>
2018-02-22 11:12:42 +0100

    Update HelloWorld.java

--- directory/HelloWorld.java
+++ directory/HelloWorld.java
@@ -2,32 +2,37 @@ package com.test;
 
 public class HelloWorld {
         public int add(int first, int second) {
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
+               try{
+               }catch(Exception e) {
+               }
+                 try{
+               }catch(Exception e) {
+               }
+                 try{
+               }catch(Exception e) {
+               }
+                 try{
+               }catch(Exception e) {
+               }
+                 try{
+               }catch(Exception e) {
+               }
+                 try{
+               }catch(Exception e) {
+               } try{
+               }catch(Exception e) {
+               }
+                 try{
+               }catch(Exception e) {
+               }
+                 try{
+               }catch(Exception e) {
+               }
         }
         
+        public int gigi(int first, int second, int first, int second,int first, int second,int first, int second,int first, int second)
+        {
+          int fourth = sixth + seventh;       
+        }
                
 }
