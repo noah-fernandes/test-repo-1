d1f948ae79df009a1ab87015e331d946577e6c51
Mircea Cadariu <cadariu.mircea@gmail.com>
2018-02-22 11:07:58 +0100

    Update HelloWorld.java

--- directory/HelloWorld.java
+++ directory/HelloWorld.java
@@ -1,54 +1,8 @@
 package com.test;
 
 public class HelloWorld {
-        public int add(int first, int second, int second, int second, int second, int second, int second) {
-          try {
-          } catch(Exception e) {
-          }
-                
-                 try {
-          } catch(Exception e) {
-          }
-                
-                 try {
-          } catch(Exception e) {
-          }
-                
-                 try {
-          } catch(Exception e) {
-          }
-                 try {
-          } catch(Exception e) {
-          }
-                
-                 try {
-          } catch(Exception e) {
-          }
-                
-                 try {
-          } catch(Exception e) {
-          }
-                
-                 try {
-          } catch(Exception e) {
-          }
-                 try {
-          } catch(Exception e) {
-          }
-                
-                 try {
-          } catch(Exception e) {
-          }
-                 try {
-          } catch(Exception e) {
-          }
-                 try {
-          } catch(Exception e) {
-          }
-                 try {
-          } catch(Exception e) {
-          }
-              
+        public int add(int first, int second) {
+                int third = first + second;
         }
         
                
