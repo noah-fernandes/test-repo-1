ad2dce5102d3d838c837229f6bc777402de72abf
Mircea Cadariu <cadariu.mircea@gmail.com>
2018-02-22 23:14:10 +0100

    Update HelloWorldTest.java

--- directory/HelloWorldTest.java
+++ directory/HelloWorldTest.java
@@ -2,50 +2,5 @@ package com.test;
 
 public class HelloWorldTest {
 
-        @Test
-        public int add(int first, int second) {
-          int third = first + second;
-          return third;
-                assertTrue(1==1)
-        }
-        
-        @Test
-        public int add(int first, int second) {
-          int third = first + second;
-          return third;
-                assertTrue(1==1)
-        }
-        
-        @Test
-        public int add(int first, int second) {
-          int third = first + second;
-          return third;
-                assertTrue(1==1)
-        }
-        
-        @Test
-        public int add(int first, int second) {
-          int third = first + second;
-          return third;
-                assertTrue(1==1)
-        }
-        
-        @Test
-        public int add(int first, int second) {
-          int third = first + second;
-          return third;
-                assertTrue(1==1)
-        }
-        @Test
-        public int add(int first, int second) {
-          int third = first + second;
-          return third;
-                assertTrue(1==1)
-        }
-        @Test
-        public int add(int first, int second) {
-          int third = first + second;
-          return third;
-                assertTrue(1==1)
-        }
+       
 }
