7a2cc693e6c292ef7f25fbdd4bea05786a12b7b5
Mircea Cadariu <cadariu.mircea@gmail.com>
2018-02-26 13:40:57 +0100

    Update HelloWorld.java

--- directory/HelloWorld.java
+++ directory/HelloWorld.java
@@ -4,71 +4,7 @@ public class HelloWorld {
    
           public int add(int first, int second) {
                 int third = first + second;
-                               
-                  
-                    int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                  
-                        int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                  
-                        int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-              
-                              
-                              }
-                  
-                   try {
-                  } catch(Exception e) {
-                  }
-   
-     
-                   try {
-                  } catch(Exception e) {
-                  }
-   
-     
-                   try {
-                  } catch(Exception e) {
-                  }
-   
-     
-                   try {
-                  } catch(Exception e) {
-                  }
-   
-     
-                   try {
-                  } catch(Exception e) {
-                  }
-   
-     
-                   try {
-                  } catch(Exception e) {
-                  }
-     
-                   try {
-                  } catch(Exception e) {
-                  }
-     
-                   try {
-                  } catch(Exception e) {
-                  }
-   
-     
-                   try {
-                  } catch(Exception e) {
-                  }
-        
+                 
         }
         
         
