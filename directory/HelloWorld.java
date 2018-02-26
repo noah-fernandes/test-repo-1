316d63886f932f38ad88b9964dd9da5806f1fee5
Mircea Cadariu <cadariu.mircea@gmail.com>
2018-02-26 13:34:50 +0100

    Update HelloWorld.java

--- directory/HelloWorld.java
+++ directory/HelloWorld.java
@@ -1,17 +1,7 @@
 package com.test;
 
 public class HelloWorld {
-        public int add(int first, int second) {
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-             
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-                int third = first + second;
-        }
-        
+   
           public int add(int first, int second) {
                 int third = first + second;
                                
