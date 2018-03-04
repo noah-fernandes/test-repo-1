28c763e9975da7eae2243015cb1a4a91d6e3621f
Mircea Cadariu <cadariu.mircea@gmail.com>
2018-03-04 22:16:53 +0100

    Update HelloWorldTest.java

--- directory/HelloWorldTest.java
+++ directory/HelloWorldTest.java
@@ -2,35 +2,7 @@ package com.test;
 
 public class HelloWorldTest {
        
-       @Test
-       public void testGigi() {
-              assertEquals(1,1);
-       }
-       
-        @Test
-       public void testGigi() {
-              assertEquals(1,1);
-       }
-       
-        @Test
-       public void testGigi() {
-              assertEquals(1,1);
-       }
-       
-        @Test
-       public void testGigi() {
-              assertEquals(1,1);
-       }
-       
-        @Test
-       public void testGigi() {
-              assertEquals(1,1);
-       }
-       
-        @Test
-       public void testGigi() {
-              assertEquals(1,1);
-       }
+      
 
        
 }
