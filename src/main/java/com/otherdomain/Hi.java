60a7e337a416a82d00b0194c8fb80946e0c62775
Mircea Cadariu <cadariu.mircea@gmail.com>
2018-03-07 18:15:51 +0100

    Update Hi.java

--- src/main/java/com/otherdomain/Hi.java
+++ src/main/java/com/otherdomain/Hi.java
@@ -1,35 +1,5 @@
 public class Hi {
-  public void sayHi() {
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-    return "gigi";
-  }
+ 
   
   public void sayHi() {
     return "gigi";
