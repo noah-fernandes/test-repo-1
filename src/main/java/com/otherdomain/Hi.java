a52cf9d639bb4fb7af172d278322daa600bef51e
Mircea Cadariu <cadariu.mircea@gmail.com>
2018-03-07 18:25:31 +0100

    Update Hi.java

--- src/main/java/com/otherdomain/Hi.java
+++ src/main/java/com/otherdomain/Hi.java
@@ -1,6 +1,30 @@
 public class Hi {
   public void sayHi() {
-    return "gigi";
+    int a = 1+2;
+        int a = 1+2;
+        int a = 1+2;
+        int a = 1+2;
+        int a = 1+2;
+        int a = 1+2;
+        int a = 1+2;
+        int a = 1+2;
+        int a = 1+2;
+        int a = 1+2;
+        int a = 1+2;
+        int a = 1+2;
+        int a = 1+2;
+        int a = 1+2;
+        int a = 1+2;
+        int a = 1+2;
+        int a = 1+2;
+        int a = 1+2;
+        int a = 1+2;
+        int a = 1+2;
+        int a = 1+2;
+        int a = 1+2;
+        int a = 1+2;
+        int a = 1+2;
+
     return "gigi";
   
   }
