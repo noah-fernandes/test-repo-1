a0ffd00d00652cf7c4ae4ff533fed9599a7d8cf7
Mircea Cadariu <cadariu.mircea@gmail.com>
2018-03-13 15:48:54 +0100

    Update Hello.java

--- src/main/java/com/nextdomain/Hello.java
+++ src/main/java/com/nextdomain/Hello.java
@@ -1,5 +1,24 @@
 public class Gigi {
   public void sayHello() {
+    int a = 1 + 1;
+    a = 1 + 1;
+        a = 1 + 1;
+        a = 1 + 1;
+        a = 1 + 1;
+        a = 1 + 1;
+        a = 1 + 1;
+        a = 1 + 1;
+        a = 1 + 1;
+        a = 1 + 1;
+        a = 1 + 1;
+        a = 1 + 1;
+        a = 1 + 1;
+        a = 1 + 1;
+        a = 1 + 1;
+        a = 1 + 1;
+        a = 1 + 1;
+        a = 1 + 1;
+        a = 1 + 1;
     return "hoi";
   }
 }
