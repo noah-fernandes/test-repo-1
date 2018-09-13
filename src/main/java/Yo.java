65190a319213e3bf5fe1a28cecd9679ae0bcdd1e
Mircea Cadariu <cadariu.mircea@gmail.com>
2018-09-13 10:26:10 +0200

    Update Yo.java

--- src/main/java/Yo.java
+++ src/main/java/Yo.java
@@ -1,5 +1,45 @@
  public class Yo {
   public void sayYo() {
+   return "yo";
     return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+  }
+  
+  public void sayYoLo() {
+   return "yo";
+    return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
+   return "yo";
   }
  }
