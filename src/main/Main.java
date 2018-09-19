0aa3b06eb857758a2b36e41ed0511644f08ed1e2
Noah Fernandes <40763370+noah-fernandes@users.noreply.github.com>
2018-09-19 11:20:09 +0200

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,23 @@
 public class Main {
   public void sayMain() {
     return "hehjy";
+    return "hehjy";
+    return "hehjy";
+    return "hehjy";
+    return "hehjy";
+    return "hehjy";
+    return "hehjy";
+    return "hehjy";
+    return "hehjy";
+    return "hehjy";
+    return "hehjy";
+    return "hehjy";
+    return "hehjy";
+    return "hehjy";
+    return "hehjy";
+    return "hehjy";
+    return "hehjy";
+    return "hehjy";
+    return "hehjy";
   }
 }
